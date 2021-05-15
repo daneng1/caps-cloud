183cf4220a09dddac7aa5a01ba73c2c35b250d8e
daneng1 <engeldb@gmail.com>
2021-05-15 12:55:04 -0700

    completed lab

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # caps-cloud
+
+## Project Overview
+
+> Create a cloud version of the CAPS project using AWS SNS and SQS.
+
+### Links
+
+[Pull Request](https://github.com/daneng1/caps-cloud/pull/2)
+
+### Requirements
+
+- SNS Topic: pickup which will receive all pickup requests from vendors
+- SQS Queue (FIFO): packages which will contain all delivery requests from vendors, in order of receipt.
+- Subscribe this queue to the pickup topic so all pickups are ordered
+- SQS Queue (Standard) for each vendor (named for the vendor) which will contain all delivery notifications from the drivers
+
+
+### UML
+
+![Caps Cloud UML](caps-cloud.png)
+
+### Terminal Logs
+
+![Caps Terminal](caps-terminal.png)
--- driver/driver.js
+++ driver/driver.js
@@ -1,69 +1,35 @@
 'use strict';
+
 const { Consumer } = require('sqs-consumer');
 const { Producer } = require('sqs-producer');
 const faker = require('faker');
-const AWS = require('aws-sdk');
-AWS.config.update({ region: 'us-west-2' });
 
-let sqs = new AWS.SQS();
-
-const flowers = Producer.create({
-  queueUrl: `https://sqs.us-west-2.amazonaws.com/468963793680/flowers-queue`,
-  region: `us-west-2`,
-});
-
-const acme = Producer.create({
-  queueUrl: `https://sqs.us-west-2.amazonaws.com/468963793680/acme-queue`,
-  region: `us-west-2`,
-});
-
-const store = '';
-const bodyId = '';
-const receiptHandle = '';
-
-const flowersArn = 'arn:aws:sqs:us-west-2:468963793680:flowers-queue';
-const acmeArn = 'arn:aws:sqs:us-west-2:468963793680:acme-queue';
-
+//subscribes to packages sqs and publishes data to vendor sqs that package was delivered
 const app = Consumer.create({
   queueUrl: 'https://sqs.us-west-2.amazonaws.com/468963793680/packages',
   handleMessage: async (message) => {
-    const messageBody = message.Body;
-    console.log(message.ReceiptHandle);
-    const receipt = message.ReceiptHandle;
-    receiptHandle.push(receipt);
-    const body = JSON.parse(messageBody);
-    const storeId = JSON.parse(body.Message).vendorId;
-    // console.log(storeId);
-    const id = JSON.parse(body.Message).id;
-    bodyId.push(id);
-    store.push(storeId);
+      const msg = JSON.parse(message.Body);
+      const order = JSON.parse(msg.Message)
+      console.log("Order Picked Up: ", order);
 
-  }
+      setTimeout(async () => {
+          const producer = Producer.create({
+              queueUrl: order.vendorId,
+              region: 'us-west-2'
+          });
+
+          await producer.send({
+              id: faker.datatype.uuid(),
+              body: JSON.stringify(order)
+          });
+          console.log('--------', '\n', (`Delivered Order #${order.id} to ${order.name}`), '\n', '--------')
+          // console.log('Sending Delivery Confirmation to Vendor')
+      }, 3000)
+
+  },
+  pollingWaitTimeMs: 5000
+
 });
+
+
 app.start();
-
-setInterval(async () => {
-
-  try {
-    const message = {
-      id: faker.datatype.uuid(),
-      body: `Package ${bodyId} is DELIVERED`,
-    };
-    // console.log(store, flowersArn, acmeArn);
-    if (store === flowersArn) await flowers.send(message);
-    if (store === acmeArn) await acme.send(message);
-    // console.log(response);
-  } catch (e) {
-    console.error(e);
-  }
-
-  var params = {
-    QueueUrl: 'https://sqs.us-west-2.amazonaws.com/468963793680/packages', /* required */
-    ReceiptHandle: receiptHandle /* required */
-  };
-  sqs.deleteMessage(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data);  // successful response
-  });
-  
-}, 1000);
